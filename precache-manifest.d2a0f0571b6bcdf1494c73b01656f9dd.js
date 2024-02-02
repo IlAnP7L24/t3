b1fa642c29b0ef59dadad707e416e98e43fd96ed
IlAnP7L24 <poddubniakillia1a@gmail.com>
2024-02-02 20:52:21 +0200

    Updates

---
+++ precache-manifest.d2a0f0571b6bcdf1494c73b01656f9dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9645ab146dfe25936d79f4bd8270a873",
+    "url": "/t3/index.html"
+  },
+  {
+    "revision": "2f081d50c0ecc0d87e72",
+    "url": "/t3/static/css/main.7169b29b.chunk.css"
+  },
+  {
+    "revision": "be6d5347df5533de2035",
+    "url": "/t3/static/js/2.ac397db4.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/t3/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "2f081d50c0ecc0d87e72",
+    "url": "/t3/static/js/main.0e5686f5.chunk.js"
+  },
+  {
+    "revision": "28aff03f989fde3aa2e4",
+    "url": "/t3/static/js/runtime~main.2b680e85.js"
+  },
+  {
+    "revision": "5057681081b65378434bc80a8f9aa206",
+    "url": "/t3/static/media/circle.50576810.png"
+  },
+  {
+    "revision": "6c8a454173c94036401ad50336202a79",
+    "url": "/t3/static/media/cross.6c8a4541.png"
+  }
+]);
