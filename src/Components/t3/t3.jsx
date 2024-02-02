f5a4af83cbaef439a4e71bbbde2479ee6012562a
IlAnP7L24 <poddubniakillia1a@gmail.com>
2024-02-02 16:25:52 +0200

    Finish

--- src/Components/t3/t3.jsx
+++ src/Components/t3/t3.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import './t3.css'
 import circle_icon from '../Assets/circle.png'
 import cross_icon from '../Assets/cross.png'
@@ -10,6 +10,7 @@ console.log("Data: ", data);
 const T3 = () => {
   let [count,setCount]=useState(0);
   let [lock,setLock]=useState(false);
+  let titleRef=useRef(null);
 
   const toggle=(e,num)=>{
     if (lock){
@@ -33,52 +34,71 @@ const T3 = () => {
 const checkWin=()=>{
   if(data[0]===data[1]&&data[1]===data[2]&&data[2]!=='')
   {
-    victory(data);
+    victory(data[2]);
     console.log("Victory 1");
   }
   else if(data[3]===data[4]&&data[4]===data[5]&&data[5]!=='')
   {
-    victory(data);
+    victory(data[5]);
         console.log("Victory 2");
   }
   else if(data[6]===data[7]&&data[7]===data[8]&&data[8]!=='')
   {
-    victory(data);
+    victory(data[8]);
         console.log("Victory 3");
   }
   else if(data[0]===data[3]&&data[3]===data[6]&&data[6]!=='')
   {
-    victory(data);
+    victory(data[6]);
         console.log("Victory 4");
   }
   else if(data[1]===data[4]&&data[4]===data[7]&&data[7]!=='')
   {
-    victory(data);
+    victory(data[7]);
         console.log("Victory 5");
   }
   else if(data[2]===data[5]&&data[5]===data[8]&&data[8]!=='')
   {
-    victory(data);
+    victory(data[8]);
         console.log("Victory 6");
   }
   else if(data[0]===data[4]&&data[4]===data[8]&&data[8]!=='')
   {
-    victory(data);
+    victory(data[8]);
         console.log("Victory 7");
   }
   else if(data[2]===data[4]&&data[4]===data[6]&&data[6]!=='')
   {
-    victory(data);
+    victory(data[6]);
         console.log("Victory 8");
   }
 }
 
-const victory=()=>{setLock(true);}
+const victory=(winner)=>{
+  setLock(true);
+  if(winner==="x"){
+    titleRef.current.innerHTML=`<img src='${cross_icon}'> won.`;
+  }
+  else if (winner==="o"){
+    titleRef.current.innerHTML=`<img src='${circle_icon}'> won.`;
+  }
+}
 //console.log(setLock);
+
+const reset=()=>{
+  document.querySelectorAll(".boxes").forEach(box => {
+    box.innerHTML = "";
+  });
+  data = new Array(9).fill("");
+  setCount(0);
+  setLock(false);
+  document.querySelector(".title").innerHTML = "Tic-Tac-Toe Game In React";
+}
+
 
   return (
     <div className='container'>
-        <h1 className="title">Tic-Tac-Toe Game In <span>React</span></h1>
+        <h1 className="title" ref={titleRef}>Tic-Tac-Toe Game In <span>React</span></h1>
         <div className="board">
 
           <div className="row1">
@@ -98,7 +118,7 @@ const victory=()=>{setLock(true);}
           </div>
 
         </div>
-        <button className="reset">Reset</button>
+      <button className="reset" onClick={()=>reset()}>Reset</button>
     </div>
   )
 }
